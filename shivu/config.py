a157d65ee995035dea2de8c22ac5aedbb872214d
Declared-Pen <bedramshakya07@gmail.com>
2025-03-28 08:06:47 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,6 +5,7 @@ class Config(object):
     OWNER_ID = "7776071513"
     sudo_users = "6312154011", "1350736363"
     GROUP_ID = -1002374696109
+    Port = 8080
     TOKEN = "8058000288:AAE_jFBEH6l_LuabvVJJ9WwVzShHxNxbvGw"
     mongo_url = "mongodb+srv://itachii130506:idjVit5zIoSOL3Gv@cluster0.vmsab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
@@ -14,7 +15,6 @@ class Config(object):
     CHARA_CHANNEL_ID = "-1002374696109"
     api_id = 28682516
     api_hash = "bb43fa00569d8f4de75aad6bdeb3650d"
-    Port = 8080
     
 class Production(Config):
     LOGGER = True
