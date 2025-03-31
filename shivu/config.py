727597a6ed0ef153b85e4041f7d1c48028444ca4
Declared-Pen <bedramshakya07@gmail.com>
2025-03-31 10:36:24 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -9,8 +9,8 @@ class Config(object):
     TOKEN = "8058000288:AAE_jFBEH6l_LuabvVJJ9WwVzShHxNxbvGw"
     mongo_url = "mongodb+srv://itachii130506:idjVit5zIoSOL3Gv@cluster0.vmsab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
+    SUPPORT_CHAT = "Collect_your_waifu_support"
+    UPDATE_CHAT = "Collect_your_waifu_support"
     BOT_USERNAME = "Collect_Em_AllBot"
     CHARA_CHANNEL_ID = "-1002374696109"
     api_id = 28682516
